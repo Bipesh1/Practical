7d49a2133b18a5fda5908781ed8818a0247108e9
Bipesh <bipesh@gmail.com>
2024-05-08 15:45:07 +0545

    first commit

---
+++ practical.js
@@ -0,0 +1,9 @@
+// This is a simple JavaScript program that adds two numbers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Main program
+  const num1 = 5;
+  const num2 = 7;
+  console.log(`Adding ${num1} and ${num2}: ${add(num1, num2)}`);
