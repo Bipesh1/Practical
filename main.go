b5da2a9046b40e3858343bde6971f89fae6bddd2
Bipesh <bipesh@gmail.com>
2024-11-24 18:53:55 +0545

    main file

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"jwtgin/routes"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	userauthrouter := router.Group("/userauth")
+	userpathrouter := router.Group("/userpath")
+	routes.AuthRoutes(userauthrouter)
+	routes.UserRoutes(userpathrouter)
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+	router.Run(":" + port)
+}
